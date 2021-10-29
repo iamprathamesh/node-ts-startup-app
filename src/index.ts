5fa7f9a544d78a7de11d781209d4d7cae520c387
Prathamesh Madur <prathameshmadur11@gmail.com>
2021-10-29 10:28:10 +0530

    Created http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hi!');
+import * as http from "http";
+
+const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end("Server is running!");
+});
+
+server.listen(3000);
+console.log(`Listening on port 3000!`);
